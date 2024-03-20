fc36ce65521c051def8ec828d0ac551c51b31cb9
amankbps <amankbps@gmail.com>
2024-03-20 12:43:21 +0530

    updated comment model with ref path

--- src/models/comments.js
+++ src/models/comments.js
@@ -4,14 +4,30 @@ import mongoose from "mongoose";
 
 const commentsSchema=new mongoose.Schema(
     {
-        comments:[
+        comments:
             {
                 content:{
                     type:String,
                     retuired:true
                 }
-            }
-        ]
+            },
+
+            userId:{
+                type:mongoose.Schema.Types.ObjectId,
+                ref:'User',
+                required:true
+            },
+            onModel:{
+                type:String,
+                required:true,
+                enum:['Tweet','Comment']
+             },
+             commentable:{
+                type:mongoose.Schema.Types.ObjectId,
+                required:true,
+                refPath:'onModel'
+             }
+        
     },
     {
         timestamps:true
