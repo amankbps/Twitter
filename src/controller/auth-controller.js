a27cfa4d172a71a2d6e1e2b649c2c8870ee7c3fe
amankbps <amankbps@gmail.com>
2024-03-20 19:28:49 +0530

    added signup with encrypted password

---
+++ src/controller/auth-controller.js
@@ -0,0 +1,34 @@
+
+import UserService from "../services/user-service.js";
+
+const userService=new UserService();
+
+export const signup=async(req,res)=>{
+
+    try 
+    {
+        const response=await userService.signup({
+            email:req.body.email,
+            password:req.body.password,
+            name:req.body.name
+         });
+    
+        return res.status(201).json({
+           success:true,
+           message:'Successfully created a new User',
+           data:response,
+           err:{}
+
+        });
+     } 
+   catch (error)
+    {
+       return res.status(500).json({
+           success:false,
+           message:'something went wrong',
+           data:{},
+           err:error
+
+        });
+    }
+};
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,7 +4,7 @@ import express from 'express';
 import {createTweet,getTweet} from '../../controller/tweet-controller.js';
 import { toggleLike } from '../../controller/like-controller.js'; 
 import { createComment } from '../../controller/comment-controller.js';
-
+import { signup } from '../../controller/auth-controller.js';
 const router=express.Router();
 
 router.post('/tweets',createTweet);
@@ -12,6 +12,8 @@ router.get('/tweets/:id',getTweet);
 
 router.post('/likes/toggle',toggleLike);
 router.post('/comments',createComment);
+
+router.post('/signup',signup);
 
 
 export default router;
